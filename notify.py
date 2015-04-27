a15cadb37d37b9473e191a9bdedff4e538e52d1a
adityapb <adityapb1546@gmail.com>
2015-04-27 22:37:46 +0530

    Add timer to notify

--- notify.py
+++ notify.py
@@ -1,5 +1,6 @@
 from pync import Notifier
+import threading
 
 def Notify(matchNo, data):
+	threading.Timer(300.0, Notify).start()
 	Notifier.notify(str(data[matchNo-1]), title = "Match Notification")
-	return
