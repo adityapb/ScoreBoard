f732cbb96d55cf5fc870630d20e1b1c2005ed235
adityapb <adityapb1546@gmail.com>
2015-04-27 22:42:16 +0530

    change notify

--- notify.py
+++ notify.py
@@ -1,6 +1,7 @@
 from pync import Notifier
 import threading
+from chooseMatches import chooseMatches
 
 def Notify(matchNo, data):
 	threading.Timer(300.0, Notify).start()
-	Notifier.notify(str(data[matchNo-1]), title = "Match Notification")
+	Notifier.notify(str(data[matchNo]), title = "Match Notification")
